b5b658d0a5a2e0b4abc244daff453d1d8cf2dccf
kseniyavldk <nuncivolt@gmail.com>
2025-12-02 19:21:23 +0300

    feat: scaffold core portfolio structure and setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+} from "./components";
+
 function App() {
-  return <div>3D Developer Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-prime">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
