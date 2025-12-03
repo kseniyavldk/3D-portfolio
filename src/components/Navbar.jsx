4a4d9fff634909c21bf62f2683d263f17d670683
kseniyavldk <nuncivolt@gmail.com>
2025-12-03 19:48:25 +0300

    feat(navbar): implement responsive menu and

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,10 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -25,7 +26,34 @@ const Navbar = () => {
             Kseniya <span className="sm:block hidden">| JavaScript Dev</span>
           </p>
         </Link>
-        <p className="text-red-500">asdsa</p>
+        <ul className="list-none flex justify-end items-start flex-col gap-4">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer `}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 ronded-xl`}
+          ></div>
+        </div>
       </div>
     </nav>
   );
